093e52cfb980736bbceacab72204f5238da1cdb3
samandarrza <samandarshr@code.edu.az>
2022-11-17 01:50:46 +0400

    Created Eterna Project

---
+++ Eterna/Models/Data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Eterna.Models
+{
+    public static class Data
+    {
+        public static List<Service> Services = new List<Service>
+        {
+            new Service{Id=1,Name="Lorem Ipsum",Icon="bxl-dribbble",
+                Title="Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi"},
+            new Service{Id=2,Name="Sed ut perspiciatis",Icon="bx-file",
+                Title="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore"},
+            new Service{Id=3,Name="Magni Dolores",Icon="bx-tachometer",
+                Title="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia"},
+            new Service{Id=4,Name="Nemo Enim",Icon="bx-world",
+                Title="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis"},
+            new Service{Id=5,Name="Dele cardo",Icon="bx-slideshow",
+                Title="Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur"},
+            new Service{Id=6,Name="Divera don",Icon="bx-arch",
+                Title="Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur"},
+        };
+    }
+}
